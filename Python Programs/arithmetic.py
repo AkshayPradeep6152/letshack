cd0799d07cf266c2c43a21febedbde9486472132
Abel Koshy Mathew <55405281+abelkmathew@users.noreply.github.com>
2020-10-17 19:54:48 +0530

    Add files via upload
    Basic arithmetic operations for two numbers

---
+++ Python Programs/arithmetic.py
@@ -0,0 +1,12 @@
+print("...........Arithmetic Operations..........")
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of b: "))
+print("Sum = ",a+b)
+print("Difference = ",a-b)
+print("Product = ",a*b)
+print("Remainder = ",a%b)
+print("Quotient = ",a//b)
+print("Division = ",round(a/b,2))
+print("Exponent = ",a**b)
+
+
